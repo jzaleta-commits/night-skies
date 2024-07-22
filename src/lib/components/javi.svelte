cb1ed414835eeb8e5426e34741338ae341ee5da1
Javier Zaleta <javizaleta@proton.me>
2024-07-22 16:47:31 -0600

    feat: :egg: Add tiny easter egg

---
+++ src/lib/components/javi.svelte
@@ -0,0 +1,7 @@
+<script>
+  console.log(
+    "%c A project by jzaleta ",
+    "color: #fff; background: hsl(0, 95%, 45%); padding: 5px 0;"
+  );
+  console.log();
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
   import Confetti from "$lib/components/confetti.svelte";
+  import Javi from "$lib/components/javi.svelte";
 </script>
 
 <Confetti />
+<Javi />
