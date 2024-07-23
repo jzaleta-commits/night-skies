2eaac2f3dd5b3c43bde78551e7472cbd37420a0f
Javier Zaleta <javizaleta@proton.me>
2024-07-22 22:10:18 -0600

    feat: :sparkles: Setup three.js starfield component

---
+++ src/lib/components/scene.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { T } from "@threlte/core";
+  import { OrbitControls, Stars } from "@threlte/extras";
+
+  let autoRotate = true;
+  let rotationSpeed = 1;
+  let totalStars = 5000;
+</script>
+
+<Stars count={totalStars} />
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[1, 8]}
+  fov={75}
+  near={0.1}
+  far={1000}
+  on:create={({ ref }) => {
+    ref.lookAt(0, 0, 0);
+  }}
+>
+  <OrbitControls
+    enableDamping
+    dampingFactor={0.05}
+    enablePan={false}
+    enableZoom={true}
+    minDistance={5}
+    maxDistance={30}
+    {autoRotate}
+    autoRotateSpeed={rotationSpeed}
+  />
+</T.PerspectiveCamera>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
 <script>
+  import { Canvas } from "@threlte/core";
+  import Scene from "$lib/components/scene.svelte";
+
   import Confetti from "$lib/components/confetti.svelte";
   import Javi from "$lib/components/javi.svelte";
 </script>
+
+<main style:width="100vw" style:height="100vh">
+  <Canvas>
+    <Scene />
+  </Canvas>
+</main>
 
 <Confetti />
 <Javi />
